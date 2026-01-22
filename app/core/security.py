acf4fb764313d75d6b949528e29b2857c4de201d
zeonga1102 <zeonga1102@gmail.com>
2026-01-22 21:08:50 +0900

    FEAT: 비밀번호 해시 비교 함수 추가

--- app/core/security.py
+++ app/core/security.py
@@ -4,3 +4,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """
+    로그인 시 입력한 비밀번호와 DB에 저장된 비밀번호 해시 비교
+    """
+    return pwd_context.verify(plain_password, hashed_password)
