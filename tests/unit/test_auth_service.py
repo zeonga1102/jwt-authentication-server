6b60232844b00e8d61cac4e19e2b48fda70e5774
zeonga1102 <zeonga1102@gmail.com>
2026-01-25 18:14:00 +0900

    TEST: auth_service 유닛 테스트 작성

---
+++ tests/unit/test_auth_service.py
@@ -0,0 +1,69 @@
+import pytest
+from unittest.mock import MagicMock
+
+from sqlalchemy.orm import Session
+
+from app.services.auth_service import login_user
+from app.models.user import User
+from app.core.security import hash_password
+
+
+def test_login_user_성공(db: Session, monkeypatch):
+    password = "password"
+    email="test@example.com"
+    user = User(
+        email=email,
+        password=hash_password(password),
+        name="tester"
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    mock_redis = MagicMock()
+    monkeypatch.setattr(
+        "app.services.auth_service.redis_client",
+        mock_redis
+    )
+
+    access_token, refresh_token = login_user(
+        db,
+        email=email,
+        password=password
+    )
+
+    assert isinstance(access_token, str)
+    assert isinstance(refresh_token, str)
+
+    mock_redis.set.assert_called_once()
+
+
+def test_login_user_존재하지_않는_이메일_실패(db: Session):
+    with pytest.raises(Exception) as exc:
+        login_user(
+            db,
+            email="nope@example.com",
+            password="password"
+        )
+
+    assert exc.value.status_code == 401
+
+
+def test_login_user_잘못된_비밀번호_실패(db: Session):
+    email="test@example.com"
+    user = User(
+        email=email,
+        password=hash_password("correct-password"),
+        name="tester"
+    )
+    db.add(user)
+    db.commit()
+
+    with pytest.raises(Exception) as exc:
+        login_user(
+            db,
+            email=email,
+            password="wrong-password"
+        )
+
+    assert exc.value.status_code == 401
